76fb84603bd332226ef64a544587623c7bfbbeb5
Tyler Wilcock <tyler.l.wilcock@gmail.com>
2017-08-15 01:17:03 -0500

    Generalization of segment processing, small bug fixes

--- main.c
+++ main.c
@@ -5,8 +5,8 @@
 #define MICROSECONDS_IN_SECOND_F 1000000.0
 #define MICROSECOND_MULT_RATIO (PRESCALER * MICROSECONDS_IN_SECOND_F) / F_CPU
 
-#define LISTENING_FALLING_LED PINC0
-#define LISTENING_RISING_LED PINC3
+#define BLUE_LED PINC0
+#define RED_LED PINC3
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
@@ -31,8 +31,11 @@ bool is_nec_header(Segment header_segments[]);
 bool within_range(int range, int expected, int actual);
 int ticks_to_microseconds(int ticks);
 protocol_type protocol_from_header(Segment header_segments[], int size);
+int calculate_segment_ticks(int segment_start, int segment_end);
 void icp_listen_for_rising();
 void icp_listen_for_falling();
+void process_new_header_segment(Segment new_segment);
+void process_new_segment(Segment new_segment);
 void reset();
 
 volatile int mark_start = -1, mark_end = -1, space_start = -1, space_end = -1;
@@ -44,7 +47,7 @@ int main(void) {
     /* Set PINB1 as an output */
     DDRB = (1 << PINB1);
     
-    DDRC = (1 << LISTENING_FALLING_LED) | (1 << LISTENING_RISING_LED);
+    DDRC = (1 << BLUE_LED) | (1 << RED_LED);
 	
     /*
         ICNC1 high to turn on input capture noise canceling
@@ -67,48 +70,48 @@ int main(void) {
     */
     while (1) {
         if(mark_start != -1 && mark_end != -1) {
-            //these are both set (not -1).  let's calculate the mark length
-            int mark_length = 0;
-            if(mark_start > mark_end) {
-                //the timer must've overflown between the time the mark started and when it ended, 
-                //so let's take that in to account when calculating the mark length
-                mark_length = (MAX_16_BIT_INT - mark_start) + mark_end;
-                PORTC = (1 << LISTENING_RISING_LED); //red
-            } else {
-                mark_length = mark_end - mark_start;
-            }
+            //a new mark has been closed (start and end have event times are not -1) - let's process this new mark
+            int mark_ticks = calculate_segment_ticks(mark_start, mark_end);
             mark_start = -1;
             mark_end = -1;
-            if(selected_protocol == UNKNOWN) {
-                //this mark must be part of the header, since we haven't matched any headers to a protocol yet
-                if(free_header_index >= HEADER_ARR_SIZE) {
-                    //something went wrong, we haven't matched a header to a protocol yet
-                    //we don't want to add this mark to the header array because we'll be out of bounds.  let's reset
-                    reset();
-                } else {
-                    struct Segment new_segment;
-                    new_segment.is_mark = true;
-                    new_segment.is_space = false;
-                    new_segment.microseconds = ticks_to_microseconds(mark_length);
-                    header_segments[free_header_index++] = new_segment;
-                    selected_protocol = protocol_from_header(header_segments, sizeof(header_segments) / sizeof(header_segments[0]));
-                }
-            }
+            struct Segment new_segment;
+            new_segment.is_mark = true;
+            new_segment.is_space = false;
+            new_segment.microseconds = ticks_to_microseconds(mark_ticks);
+            process_new_segment(new_segment);
         }
+        
+        if(space_start != -1 && space_end != -1) {
+            //a new space has been closed (start and end have event times are not -1) - let's process this new space
+            int space_ticks = calculate_segment_ticks(space_start, space_end);
+            space_start = -1;
+            space_end = -1;
+            struct Segment new_segment;
+            new_segment.is_mark = false;
+            new_segment.is_space = true;
+            new_segment.microseconds = ticks_to_microseconds(space_ticks);
+            process_new_segment(new_segment);
+        }     
     }
 }
 
 ISR(TIMER1_CAPT_vect) {
     if(icp_listening_for_falling()) {
         mark_start = ICR1;
-        space_end = ICR1;
+        if(space_start != -1) {
+            //we don't want to set space_end if we haven't started timing a space yet
+            //e.g. this is the first pulse of a full burst, and it is falling (the start of a mark in a TSOP382)
+            space_end = ICR1;
+        }
         icp_listen_for_rising(); 
-        //PORTC = (1 << LISTENING_RISING_LED); //blue
     } else if(icp_listening_for_rising()) {
-        mark_end = ICR1;
+        if(mark_start != -1) {
+            //we don't want to set mark_end if we haven't started timing a mark yet
+            //e.g. this is the first pulse of a full burst, and it is rising
+            mark_end = ICR1;
+        }
         space_start = ICR1;
         icp_listen_for_falling();
-        //PORTC = (1 << LISTENING_FALLING_LED); //red
     }        
 }
 
@@ -139,7 +142,7 @@ protocol_type protocol_from_header(Segment header_segments[], int size) {
     if(size == 1) return UNKNOWN;
     if(size == 2) {
         if(is_nec_header(header_segments)) {
-            selected_protocol = NEC;
+            return NEC;
         }
     }
     return UNKNOWN;
@@ -149,21 +152,54 @@ int ticks_to_microseconds(int ticks) {
     return ticks * MICROSECOND_MULT_RATIO;
 }
 
+int calculate_segment_ticks(int segment_start, int segment_end) {
+    if(segment_start > segment_end) {
+        //the timer must've overflown between the time the segment started and when it ended,
+        //so let's take that in to account when calculating the segment length
+        return (MAX_16_BIT_INT - segment_start) + segment_end;
+    }
+    return segment_end - segment_start;
+}
+
+
 bool is_nec_header(Segment header_segments[]) {
     if(header_segments[0].is_mark && within_range(125, 9000, header_segments[0].microseconds)
-       && header_segments[0].is_space && within_range(125, 4500, header_segments[1].microseconds) {
+       && header_segments[1].is_space && within_range(125, 4500, header_segments[1].microseconds)) {
         return true;
     }
     return false;
 }
 
 bool within_range(int range, int expected, int actual) {
-    int max = range + expected;
     int min = expected - range;
+    int max = range + expected;
     if(actual < min || actual > max) return false;
     return true;
 }
 
+void process_new_segment(Segment new_segment) {
+    if(selected_protocol == UNKNOWN) {
+        //this segment must be part of the header since we haven't matched the header to a protocol yet
+        process_new_header_segment(new_segment);
+    } else if(selected_protocol == NEC) {
+        //this is a logical data segment for the NEC protocol
+    } else {
+        //this probably shouldn't happen - let's turn on our red led to indicate we're in the dangerzone
+        PORTC |= (1 << RED_LED);
+    }
+}
+
+void process_new_header_segment(Segment new_segment) {
+    if(free_header_index >= HEADER_ARR_SIZE) {
+        //something went wrong, we haven't matched a header to a protocol yet
+        //we don't want to add this mark to the header array because we'll be out of bounds.  let's reset
+        reset();
+    } else {
+        header_segments[free_header_index++] = new_segment;
+        selected_protocol = protocol_from_header(header_segments, sizeof(header_segments) / sizeof(header_segments[0]));
+    }
+}
+
 void reset() {
-    
+    //ignore all IR pulses for set period (e.g. 300ms) to wait out the current borked pulse and reset all variables to get a clean slate
 }
